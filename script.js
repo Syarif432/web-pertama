151dc211a4b6118929f4027d15e8bd9413f204c6
syarif402 <jorok432@gmail.com>
2022-08-27 16:36:44 +0700

    My first website

---
+++ script.js
@@ -0,0 +1,7 @@
+const redirInstagram = () => {
+    window.location.href = "https://www.instagram.com/bogeng_432/"
+}
+
+const redirWhatsapp = () => {
+    window.location.href = "https://wa.me/+6281358722785"
+}
